db344d0079942359b3f53bb17c8e9d0e67f83732
Patrick Quinn <patrick@element84.com>
2015-09-03 17:19:51 -0400

    WIP: Use gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,62 +1,44 @@
 var gulp        = require('gulp'),
-    browserify  = require('browserify');
+    browserify  = require('browserify'),
+    source      = require('vinyl-source-stream'),
+    del         = require('del'),
+    less        = require('gulp-less'),
+    sourcemaps  = require('gulp-sourcemaps'),
+    buffer      = require('vinyl-buffer'),
+    uglify      = require('gulp-uglify'),
+    gutil       = require('gulp-util'),
+    rename      = require("gulp-rename");
 
-gulp.task('compass', function() {
-  return gulp.src('./src/stylesheets/**/*.{scss,sass}')
-    .pipe($.plumber())
-    .pipe($.compass({
-      css: 'dist/stylesheets',
-      sass: 'src/stylesheets'
-    }))
-    .pipe(gulp.dest('dist/stylesheets'));
+gulp.task('clean', function() {
+  return del(['./dist', './tmp']);
 });
 
-gulp.task('js', ['coffee'], function(){
-  return gulp.src([
-        'src/scripts/namespaces.js',
-        'dist/js/latlng.js.js',
-        'dist/js/coordinate.js.js',
-        'dist/js/arc.js.js',
-        'dist/js/geoutil.js.js',
-        'dist/js/spherical_polygon.js.js'
-      ])
-    .pipe(uglify())
-    .pipe(concat('divide-polygon.js'))
-    .pipe(gulp.dest('./dist/'));
+gulp.task('less', function () {
+  return gulp.src('./src/css/timeline.less')
+    .pipe(sourcemaps.init())
+    .pipe(less())
+    .pipe(rename('edsc-timeline.css'))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('./dist/css/'));
 });
 
-gulp.task('coffee', function() {
-  return gulp.src('src/scripts/*.js.coffee')
-      .pipe(coffee())
-      .pipe(gulp.dest('./dist/js/'));
-});
-
-gulp.task('clean', function(cb) {
-  del('./dist', cb);
-});
-
-gulp.task('images', function() {
-  return gulp.src('./src/images/**/*')
-    .pipe($.imagemin({
-      progressive: true
-    }))
-    .pipe(gulp.dest('./dist/images'))
-})
-
-gulp.task('templates', function() {
-  return gulp.src('src/**/*.html')
-    .pipe($.plumber())
-    .pipe( gulp.dest('dist/') )
-});
-
-gulp.task('foo', function() {
-  return browserify({
+gulp.task('compile', function() {
+  var b = browserify({
     entries: ['./src/js/timeline.coffee'],
-    extensions: ['.coffee']
-  }).bundle();
+    extensions: ['.coffee'],
+    debug: true
+  });
 
+  return b.bundle()
+    .pipe(source('edsc-timeline.min.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(uglify())
+    .on('error', gutil.log)
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./dist/js/'));
 });
 
-gulp.task('build', ['compass', 'js', 'templates', 'images']);
+gulp.task('build', ['compile', 'less']);
 
 gulp.task('default', ['build']);
