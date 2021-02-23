e982dbce78ba286d8436b3f0b42f25eb28bcf0cb
Ryan Abbott <ryan@element84.com>
2021-02-23 12:31:26 -0500

    EDSC-3025: Cleans up and adds docs to utilities

--- src/utils/getPositionByTimestamp.js
+++ src/utils/getPositionByTimestamp.js
@@ -1,15 +1,27 @@
 import { determineScaledSize } from './determineScaledSize'
 import { getIntervalsDuration } from './getIntervalsDuration'
 
-export const getPositionByTimestamp = (timestamp, timeIntervals, zoomLevel, wrapperWidth) => {
+/**
+ * Determine the css position
+ * @param {Object} param0
+ * @param {Integer} param0.timestamp The timestamp to determine the position of
+ * @param {Array} param0.timeIntervals Array of dates representing intervals at the provided zoom level
+ * @param {Integer} param0.zoomLevel Current zoom level of the timeline
+ * @param {Integer} param0.wrapperWidth Width (in pixels) of the DOM element that wraps the timeline intervals
+ */
+export const getPositionByTimestamp = ({
+  timestamp,
+  timeIntervals,
+  zoomLevel,
+  wrapperWidth
+}) => {
   const totalDuration = getIntervalsDuration(timeIntervals, zoomLevel)
 
   const timestampDuration = timestamp - timeIntervals[0]
 
   const timestampPercentage = timestampDuration / totalDuration
+
   const width = determineScaledSize(totalDuration, zoomLevel, wrapperWidth)
 
-  const result = width * timestampPercentage
-
-  return result
+  return width * timestampPercentage
 }
--- src/utils/getTemporalByPosition.js
+++ src/utils/getTemporalByPosition.js
@@ -1,5 +1,13 @@
 import { calculateTimeIntervals } from './calculateTimeIntervals'
 
+/**
+ * Determine the temporal value at an exact pixel location
+ * @param {Object} param0
+ * @param {Object} param0.position Position of the left side of the timeline DOM element in pixels
+ * @param {Object} param0.timeIntervals Array of dates representing intervals at the provided zoom level
+ * @param {Object} param0.zoomLevel Current zoom level of the timeline
+ * @param {Object} param0.intervalListWidthInPixels Width (in pixels) of the DOM element that holds the timeline intervals
+ */
 export const getTemporalByPosition = ({
   position,
   timeIntervals,
@@ -8,7 +16,12 @@ export const getTemporalByPosition = ({
 }) => {
   const startTime = timeIntervals[0]
   const lastInterval = timeIntervals[timeIntervals.length - 1]
-  const [endTime] = calculateTimeIntervals(lastInterval, zoomLevel, 1, false)
+  const [endTime] = calculateTimeIntervals({
+    timeAnchor: lastInterval,
+    zoomLevel,
+    numIntervals: 1,
+    reverse: false
+  })
 
   const percentScrolled = ((position) / intervalListWidthInPixels)
   const timestamp = startTime + ((endTime - startTime) * percentScrolled)
