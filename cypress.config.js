407875a8619feb87c5e5141ddab95d89c161e5d1
Matthew Crouch <matthew@element84.com>
2023-03-01 11:02:25 -0500

    EDSC-3673: Adds eslint job to github actions

--- cypress.config.js
+++ cypress.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable global-require */
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
@@ -7,8 +8,8 @@ module.exports = defineConfig({
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
+      return require('./cypress/plugins/index')(on, config)
     },
-    baseUrl: 'http://localhost:3011/#',
-  },
+    baseUrl: 'http://localhost:3011/#'
+  }
 })
--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable global-require */
 /// <reference types="cypress" />
 // ***********************************************************
 // This example plugins/index.js can be used to load plugins
